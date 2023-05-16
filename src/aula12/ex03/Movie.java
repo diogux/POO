71d477c42b8a79df28216539a1e912281e77a546
diogux <diogomiguel.fernandes@ua.pt>
2023-05-16 17:59:16 +0100

    xd

--- src/aula12/ex03/Movie.java
+++ src/aula12/ex03/Movie.java
@@ -1,5 +1,29 @@
 package aula12.ex03;
 
-public class Movie {
-    
+public class Movie { 
+    private String title;
+    private int runningTime;
+    private double score;
+    private String rating;
+    private String genre;
+
+    public Movie(String title, int runningTime, double score, String rating, String genre) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.score = score;
+        this.rating = rating;
+        this.genre = genre;
+    }
+
+    public String getTitle() {return title;}
+    public int getRunningTime() {return runningTime;}
+    public double getScore() {return score;}
+    public String getRating() {return rating;}
+    public String getGenre() {return genre;}
+
+    @Override
+    public String toString() {
+        return String.format("Title: %s Running Time: %d Score: %.1f Rating: %s Genre: %s ", title, runningTime, score, rating, genre);
+    }
+
 }
--- src/aula12/ex03/MovieOrganizer.java
+++ src/aula12/ex03/MovieOrganizer.java
@@ -1,5 +1,30 @@
 package aula12.ex03;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+
 
 public class MovieOrganizer {
-    
+    TreeMap<String, Movie> movies = new TreeMap<>();
+
+    public void loadMovies(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;
+        reader.readLine(); // skip header
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split("\t");
+            String title = parts[0];
+            int runningTime = Integer.parseInt(parts[1]);
+            double score = Double.parseDouble(parts[2]);
+            String rating = parts[3];
+            String genre = parts[4];
+            movies.put(title, new Movie(title, runningTime, score, rating, genre));
+        }
+        reader.close();
+    }
+
+
+
+
 }
